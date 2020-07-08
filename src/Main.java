b73522489b599ba0e0d66733a1bd047f5320b357
Tzeentche <ilyasugako87@gmail.com>
2020-07-08 23:37:59 +0300

    Refactored class "Main".

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Pattern pattern = Pattern.compile("[a-z]+");
+        Pattern pattern1 = Pattern.compile("[a-z]+");
+
+        Matcher matcher1 = pattern1.matcher("a b c d 1 2 3 4");
+        System.out.println(matcher1.find());
+
+        Matcher matcher2 = pattern1.matcher("A B C D 1 2 3 4");
+        System.out.println(matcher2.find());
+
+        Pattern pattern2 = Pattern.compile("[a-zA-Z0-9]");
+
+        Matcher matcher3 = pattern2.matcher("A B C D a b c d 1 2 3 4");
+        System.out.println(matcher3.find());
     }
 }
