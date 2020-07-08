5b4b96e2bee4368d51df50c715858f1672f2d91d
Tzeentche <ilyasugako87@gmail.com>
2020-07-08 23:43:44 +0300

    Refactored class "Main".

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Pattern pattern1 = Pattern.compile("[a-z]+");
+        Pattern pattern = Pattern.compile("[\\S]");
 
-        Matcher matcher1 = pattern1.matcher("a b c d 1 2 3 4");
+        Matcher matcher1 = pattern.matcher("a b c d e f g h");
         System.out.println(matcher1.find());
 
-        Matcher matcher2 = pattern1.matcher("A B C D 1 2 3 4");
+        Matcher matcher2 = pattern.matcher("f g h a b c");
         System.out.println(matcher2.find());
 
-        Pattern pattern2 = Pattern.compile("[a-zA-Z0-9]");
-
-        Matcher matcher3 = pattern2.matcher("A B C D a b c d 1 2 3 4");
+        Matcher matcher3 = pattern.matcher("123");
         System.out.println(matcher3.find());
+
+//        Pattern pattern1 = Pattern.compile("[a-z]+");
+//
+//        Matcher matcher1 = pattern1.matcher("a b c d 1 2 3 4");
+//        System.out.println(matcher1.find());
+//
+//        Matcher matcher2 = pattern1.matcher("A B C D 1 2 3 4");
+//        System.out.println(matcher2.find());
+//
+//        Pattern pattern2 = Pattern.compile("[a-zA-Z0-9]");
+//
+//        Matcher matcher3 = pattern2.matcher("A B C D a b c d 1 2 3 4");
+//        System.out.println(matcher3.find());
     }
 }
